5672162a624a6768728f61b9be12dd976f873b4c
minhm23 <minhmai2304@gmail.com>
2022-06-01 11:54:23 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+library(plotly)
+library(scales)
+
+
+collisions_df <- read.csv("seattle-collisions.csv", stringsAsFactors = F)
 
 server <- function(input, output) {
   output$intro <- renderText({
